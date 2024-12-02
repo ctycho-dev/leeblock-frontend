18a203061f75e10a521c13d1e6f73e490267fc46
ctycho-dev <ctycho23@gmail.com>
2024-12-02 22:22:52 +0300

    deploy.sh updated

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-# Pull the latest changes from the repository
-echo "Pulling latest changes from git..."
-docker-compose down
+
+git restore .
 git pull origin main
 
-# Rebuild the Docker image and restart the container
-echo "Rebuilding Docker image and restarting container..."
 docker build . -t leeblock_react
 
-# docker-compose build --no-cache
+docker-compose down
 docker-compose up -d
 
-# Output the running status
 docker-compose ps
 
-echo "Deployment complete!"
